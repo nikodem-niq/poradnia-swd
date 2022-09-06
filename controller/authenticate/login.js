f34d9fb55b61c3b681a337c985d0842bf455b123
unknown <nikodem.zychma@gmail.com>
2022-09-07 01:16:13 +0800

    feat: pin

--- controller/authenticate/login.js
+++ controller/authenticate/login.js
@@ -1,5 +1,5 @@
 const login = pin => {
-    return [pin == process.env.PIN || pin == process.env.PIN2 || pin == process.env.PIN3, pin];
+    return [pin == process.env.PIN || pin == process.env.PIN2 || pin == process.env.PIN3 || pin == process.env.PIN4 || pin == process.env.PIN5 || pin == process.env.PIN6 || pin == process.env.PIN7 || pin == process.env.PIN8 || pin == process.env.PIN9, pin];
 }
 
 
