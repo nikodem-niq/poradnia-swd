97d38d07afe48c942c5203f2ca50fc022354c65e
N. Zychma <nikodem.zychma@gmail.com>
2024-10-03 18:29:10 +0200

    dev

--- controller/restart.js
+++ controller/restart.js
@@ -1,6 +1,6 @@
 const { exec } = require('child_process');
 //const forever = require('forever');
-
+console.log('test')
 const restartController = (req,res,next) => {
     if(req.cookies["userData"]) {
         exec('', (error, stdout, stderr) => {
