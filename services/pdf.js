c5a71db4f4edf0c267bce5c3bb6252d6fb9af507
N. Zychma <nikodem.zychma@gmail.com>
2024-10-05 14:57:11 +0200

    improvement

--- services/pdf.js
+++ services/pdf.js
@@ -1,48 +1,31 @@
-import puppeteer from 'puppeteer';
 import Handlebars from 'handlebars';
 import fs from 'fs';
 import path from 'path';
+import pdf from 'html-pdf';
 
 export const generatePdf = async (formData) => {
   try {
-    // Read and compile the Handlebars template
     const source = fs.readFileSync(path.join(process.cwd(), "/public/views/email/emailTemplate.hbs"), "utf8");
     const template = Handlebars.compile(source);
     const html = template({formData});
 
-    // Launch a new browser instance
-    const browser = await puppeteer.launch({
-      headless: "new",  // Use new headless mode
-      args: ['--no-sandbox', '--disable-setuid-sandbox'] // Needed for some server environments
-    });
+    const pdfOptions = {
+      "format": "A4",
+      "orientation": "landscape",
+      "border" : "0px"
+    }
 
-    // Create a new page
-    const page = await browser.newPage();
-
-    // Set the HTML content of the page
-    await page.setContent(html, {
-      waitUntil: 'networkidle0' // Wait until the network is idle
-    });
-
-    // Generate PDF
-    const pdfBuffer = await page.pdf({
-      format: 'A4',
-      landscape: true,
-      printBackground: true,
-      margin: {
-        top: '0px',
-        right: '0px',
-        bottom: '0px',
-        left: '0px'
-      }
-    });
-
-    // Close the browser
-    await browser.close();
+    const pdfBuffer = new Promise((resolve, reject) => {
+      pdf.create(html, pdfOptions).toBuffer((err, buffer) => {
+        if(err) reject();
+        resolve(buffer);
+      })
+    })
+    const pdfFile = await pdfBuffer;
 
     console.log('PDF generated successfully. Buffer length:', pdfBuffer.length);
     
-    return pdfBuffer;
+    return pdfFile;
   } catch (error) {
     console.error('Error in generatePdf:', error);
     throw error;
